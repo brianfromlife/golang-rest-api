d2711c6dd686f1a4dbd8a5a14f92bcef58f198e8
bbates <bbates@captechventures.com>
2021-05-03 12:21:49 -0400

    Update users_test.go

--- internal/api/users_test.go
+++ internal/api/users_test.go
@@ -41,3 +41,33 @@ func TestRegisterAccount_UsernameExists(t *testing.T) {
 	assert.Equal(t, http.StatusBadRequest, rec.Code)
 
 }
+
+func TestRegisterAccount_PasswordTooShort(t *testing.T) {
+	// test data
+	testUser := `{"username":"user1","email":"123"}`
+
+	// echo setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(testUser))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// internal setup
+	cfg := &config.Settings{}
+	mockUserData := usermocks.NewUserDataStoreMock()
+	userSvc := services.NewUserService(cfg, mockUserData)
+
+	mockApp := App{
+		userSvc: userSvc,
+	}
+
+	// act
+	mockApp.Register(c)
+
+	// assert
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+	assert.Contains(t, rec.Body.String(), "A validation error occurred")
+	assert.Contains(t, rec.Body.String(), "Password must be 8 characters")
+
+}
